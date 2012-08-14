3060493e0d15db483fa92c2cc292ebe37896b0e9
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-08-14 03:40:36 -0400

    Added authors & license files; added copyright comments to code

--- grammar.go
+++ grammar.go
@@ -1,3 +1,7 @@
+// Copyright 2012 The Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // The grammar package randomly generates sentences based on a user-supplied context-free grammar.
 package grammar
 
